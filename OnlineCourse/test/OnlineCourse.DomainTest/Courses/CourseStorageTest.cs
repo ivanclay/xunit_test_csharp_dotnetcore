52d01418eb885d6c7b0ec79220ba46e8b777b1a5
ivanclay <ivan.clay.moura@gmail.com>
2022-01-09 19:57:25 -0300

    Mock.

---
+++ OnlineCourse/test/OnlineCourse.DomainTest/Courses/CourseStorageTest.cs
@@ -0,0 +1,62 @@
+﻿using Moq;
+using OnlineCouse.Domain.Courses;
+using System;
+using Xunit;
+
+namespace OnlineCourse.DomainTest.Courses
+{
+    public class CourseStorageTest
+    {
+        [Fact]
+        public void MustAddCourse()
+        {
+            var courseDto = new CourseDto
+            {
+                Name = "Name",
+                Description = "Description",
+                Duration = 80,
+                Audience = 0,
+                Cost = 100
+            };
+
+            var courseRepositoryMock = new Mock<ICourseRepository>();
+
+            var courseStorage = new CourseStorage(courseRepositoryMock.Object);
+
+            courseStorage.ToStorage(courseDto);
+
+            courseRepositoryMock.Verify(r => r.ToStorage(It.IsAny<Course>()));
+        }
+
+
+    }
+
+    public interface ICourseRepository 
+    {
+        void ToStorage(Course course);
+    }
+    public class CourseStorage
+    {
+        private ICourseRepository _courseRepository;
+
+        public CourseStorage(ICourseRepository courseRepository)
+        {
+            this._courseRepository = courseRepository;
+        }
+
+        public void ToStorage(CourseDto courseDto)
+        {
+            var course = new Course(courseDto.Name, courseDto.Description,courseDto.Duration, Audience.Estudante, courseDto.Cost);
+            _courseRepository.ToStorage(course);
+        }
+    }
+
+    public class CourseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Duration { get; set; }
+        public int Audience { get; set; }
+        public int Cost { get; set; }
+    }
+}
